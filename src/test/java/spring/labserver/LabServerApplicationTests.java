0ad0ba4e640a5bfffe7f01d5e5961e969ed7d63e
comstering <coms1101@naver.com>
2022-03-07 10:13:12 +0900

    🎉 feat: Start Lab Spring Server

---
+++ src/test/java/spring/labserver/LabServerApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.labserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
