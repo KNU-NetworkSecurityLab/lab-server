e4d59bc3835dee65bedd70626db7a1f41277bb03
comstering <coms1101@naver.com>
2022-03-07 10:38:56 +0900

    ✅ test: Add Contorller Test

---
+++ src/test/java/spring/labserver/ControllerTest.java
@@ -0,0 +1,27 @@
+package spring.labserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import spring.labserver.controllers.MainController;
+
+@WebMvcTest(controllers = MainController.class)
+public class ControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testHome() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World!"));
+    }
+    
+}
